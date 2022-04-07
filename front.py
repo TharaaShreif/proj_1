22da4da6cddffbfd6ef4f66bf7aaebf234954a79
Anwar Abdallah <71230137+anwar28499@users.noreply.github.com>
2022-04-07 08:51:23 +0300

    front.py
    front

--- front.py
+++ front.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 #getting the books info request to catalogServer
 @app.route('/search/<topic>', methods=['GET'])
 def search(topic): 
-  result = request.get("http://172.16.224.54:5000/search/"+str(topic)) #send request to catalog server
+  result = request.get("http://127.0.0.1:5000/search/"+str(topic)) #send request to catalog server
   return (result.content)
 
 
-#============================== 2- info operation ====================================
+#info operation 
 #send request to the cataloge server to get information about id book
 @app.route('/info/<int:bookID>', methods=['GET'])
 def get_info(bookID):
@@ -21,41 +21,33 @@ def get_info(bookID):
   result = request.get("http://172.16.224.54:5000/info/"+str(bookID))
   return (result.content)
 
-#============================= 3- update price ========================================
-
 #update the price of a book 
+
 @app.route('/update_price/<int:bookID>', methods=['PUT'])
 def update_book_price(bookID):
   price = request.json['price']
   #tharaa vm
   result = request.put("http://172.19.224.20:5000/update_price/"+str(bookID),data={'price':price})
   return (result.content)
-
-#============================= 4- update quantity ========================================
-
-            #=================== increease quantity =================
- 
+#update quantity
+#1increease
 @app.route('/increase_quantity/<int:bookID>', methods=['PUT'])
 def increase_book_quantity(bookID):
+      
   new_amount = request.json['new_amount']
    #tharaa pc
   result = request.put("http://172.19.224.54:5000/increase_quantity/"+str(bookID),data={'new_amount':new_amount})
   return (result.content)
-
-            #=================== decreease quantity =================
+#2decreease
  
 @app.route('/decrease_quantity/<int:bookID>', methods=['PUT'])
 def decrease_book_quantity(bookID):
   new_amount = request.json['new_amount']
   result = Request.put("http://172.19.224.54:5000/decrease_quantity/"+str(bookID),data={'new_amount':new_amount})
   return (result.content)
+#Order server requests
 
-
-#==============================================================================================
-#============================ Order server requests ===========================================
-#==============================================================================================
-
-#====================== purchase ====================================================
+#purchase 
 @app.route('/purchase/<int:bookID>', methods=['PUT'])
 def purchase(bookID):
     #tharaa vm
